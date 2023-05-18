e2b7a533629e9ca94dca5e1a468d8149684ed39b
Win <win@wins-mac-mini.home>
2023-05-17 21:52:49 -0400

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
                     learning and delivering projects to the highest standards of customer excellence.</p>
                 <div>
                     <button  className='text-white group border-2 px-6 py-3 my-2 items-center flex hover:bg-pink-600 hover:border-pink-600'>View Work <Link to="work" smooth={true} duration={500}>
-                        Work
+                     
                     </Link>
                         <span className='group-hover:rotate-90 duration-300'>
                             <HiArrowNarrowRight  className='ml-3' />
